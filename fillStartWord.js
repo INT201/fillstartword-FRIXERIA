16dab0804104e8cf522cc348c9bec137e0bf37c7
FRIXERIA <pattharaphon.saef@kmutt.ac.th>
2022-09-11 11:56:44 +0700

    Update#1

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,7 +1,12 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
- git test
- ..
+ if (word===null||word===undefined) {
+    return undefined
+ } else if (condition) {
+    
+ } else {
+    return startWord+word
+ }
 }
 module.exports = fillStartWord
